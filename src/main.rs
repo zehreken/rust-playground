f3e54aa2e67609ff06a19243d79e5ca02ac80078
zehreken <zehreken@gmail.com>
2019-09-01 23:02:59 +0200

    Add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-
 fn main()
 {
-	let stdout = stdout();
-	let out = b"Hello fellow Rustaceans!";
-	let width = 24;
+	let logical: bool = true;
+	let a_float: f64 = 1.0;
+	let mut an_integer = 5i32; // Suffix annotation
 
-	let mut writer = BufWriter::new(stdout.lock());
-	say(out, width, &mut writer).unwrap();
+	println!("{}", logical);
+	println!("{}", a_float);
+	println!("{}", an_integer);
+
+	an_integer = 6;
+	println!("{}", an_integer);
+	an_integer += 1;
+	println!("{}", an_integer);
+
+	let array: [i32; 5] = [1, 2, 3, 4, 5];
+	println!("{}", array.len());
+
+	let slice = &array[1 .. 3];
+	println!("{}, {}", slice[0], slice.len());
 }
