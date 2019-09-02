1283a7ed311a2ef1910648b81ae6f908990a1262
zehreken <zehreken@gmail.com>
2019-09-02 21:18:16 +0200

    Do clean-up

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,6 @@ extern crate sdl2;
 
 fn main()
 {
-	let logical: bool = true;
-	let a_float: f64 = 1.0;
-	let mut an_integer = 5i32; // Suffix annotation
-
-	println!("{}", logical);
-	println!("{}", a_float);
-	println!("{}", an_integer);
-
-	an_integer = 6;
-	println!("{}", an_integer);
-	an_integer += 1;
-	println!("{}", an_integer);
-
-	let array: [i32; 5] = [1, 2, 3, 4, 5];
-	println!("{}", array.len());
-
-	let slice = &array[1 .. 3];
-	println!("{}, {}", slice[0], slice.len());
-	
 	let sdl = sdl2::init().unwrap();
 	let video_subsystem = sdl.video().unwrap();
 	let window = video_subsystem
