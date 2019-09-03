d7bbd20c55612db8edda0f85cdf917f2f49a210f
zehreken <zehreken@gmail.com>
2019-09-03 22:57:03 +0200

    Add rectangle drawing and some basic animation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate sdl2;
+extern crate rand;
 
 use sdl2::pixels::Color;
 use std::time::Duration;
+use sdl2::rect::Rect;
+use rand::Rng;
 
 fn main()
 {
+	let mut rng = rand::thread_rng();
+
 	let sdl_context = sdl2::init().unwrap();
 	let video_subsystem = sdl_context.video().unwrap();
 	let window = video_subsystem
@@ -12,14 +17,18 @@ fn main()
 		.position_centered()
 		.build()
 		.unwrap();
-	
+
 	let mut canvas = window.into_canvas().build().unwrap();
 	canvas.set_draw_color(Color::RGB(0, 255, 255));
 	canvas.clear();
 	canvas.present();
 
-	let mut event_pump = sdl_context.event_pump().unwrap();
+	let row_count = 60;
+	let column_count = 80;
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
 	let mut i = 0;
+
 	'running: loop
 	{
 		for event in event_pump.poll_iter()
@@ -35,6 +44,19 @@ fn main()
 		i = (i + 1) % 255;
 		canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
 		canvas.clear();
+
+        for row in 0..row_count
+		{               
+			for column in 0..column_count
+			{
+				if rng.gen_range(0, 2) == 0
+				{
+					canvas.set_draw_color(Color::RGB(0, 0, 0));
+					canvas.fill_rect(Rect::new(column * 10, row * 10, 10, 10));
+				}
+			}
+		}
+
 		canvas.present();
 
 		::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
