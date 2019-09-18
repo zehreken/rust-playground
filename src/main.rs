38bfdf1a2fe19190e035ec5ca3075b74d69fd2cb
zehreken <zehreken@gmail.com>
2019-09-18 21:48:48 +0200

    Clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::time::Duration;
 mod grid;
 pub use crate::grid::grid_config;
 
-const moore_directions: [Point; 8] = [
+const MOORE_DIRECTIONS: [Point; 8] = [
     Point { x: -1, y: -1 },
     Point { x: -1, y: 0 },
     Point { x: -1, y: 1 },
@@ -36,36 +36,36 @@ struct Cell {
 fn calculate_neighbours(x: i32, y: i32) -> [Point; 8] {
     let neighbours: [Point; 8] = [
         Point {
-            x: moore_directions[0].x + x,
-            y: moore_directions[0].y + y,
+            x: MOORE_DIRECTIONS[0].x + x,
+            y: MOORE_DIRECTIONS[0].y + y,
         },
         Point {
-            x: moore_directions[1].x + x,
-            y: moore_directions[1].y + y,
+            x: MOORE_DIRECTIONS[1].x + x,
+            y: MOORE_DIRECTIONS[1].y + y,
         },
         Point {
-            x: moore_directions[2].x + x,
-            y: moore_directions[2].y + y,
+            x: MOORE_DIRECTIONS[2].x + x,
+            y: MOORE_DIRECTIONS[2].y + y,
         },
         Point {
-            x: moore_directions[3].x + x,
-            y: moore_directions[3].y + y,
+            x: MOORE_DIRECTIONS[3].x + x,
+            y: MOORE_DIRECTIONS[3].y + y,
         },
         Point {
-            x: moore_directions[4].x + x,
-            y: moore_directions[4].y + y,
+            x: MOORE_DIRECTIONS[4].x + x,
+            y: MOORE_DIRECTIONS[4].y + y,
         },
         Point {
-            x: moore_directions[5].x + x,
-            y: moore_directions[5].y + y,
+            x: MOORE_DIRECTIONS[5].x + x,
+            y: MOORE_DIRECTIONS[5].y + y,
         },
         Point {
-            x: moore_directions[6].x + x,
-            y: moore_directions[6].y + y,
+            x: MOORE_DIRECTIONS[6].x + x,
+            y: MOORE_DIRECTIONS[6].y + y,
         },
         Point {
-            x: moore_directions[7].x + x,
-            y: moore_directions[7].y + y,
+            x: MOORE_DIRECTIONS[7].x + x,
+            y: MOORE_DIRECTIONS[7].y + y,
         },
     ];
 
@@ -89,7 +89,6 @@ fn get_live_neighbour_count(
         }
     }
 
-    //println!("neighbour count: {}", neighbour_count);
     return neighbour_count;
 }
 
@@ -98,9 +97,8 @@ fn cell_tick(
     grid: [[Cell; grid_config::COLUMN_COUNT as usize]; grid_config::ROW_COUNT as usize],
 ) -> Cell {
     let live_neighbour_count: i32 = get_live_neighbour_count(cell, grid);
-    //println!("before: {}", cell.current_state);
+
     if cell.current_state == 1 {
-        //let live_neighbour_count: i32 = get_live_neighbour_count(cell, grid);
         if live_neighbour_count < 2 {
             cell.future_state = 0;
         } else if live_neighbour_count == 2 || live_neighbour_count == 3 {
@@ -109,14 +107,12 @@ fn cell_tick(
             cell.future_state = 0;
         }
     } else {
-        //let live_neighbour_count: i32 = get_live_neighbour_count(cell, grid);
         if live_neighbour_count == 3 {
             cell.future_state = 1;
         } else {
             cell.future_state = 0;
         }
     }
-    //println!("after: {}", cell.current_state);
 
     return cell;
 }
@@ -139,7 +135,6 @@ fn create_cell() -> Cell {
 }
 
 fn main() {
-    let mut step_count: i32 = 0;
     let mut rng = rand::thread_rng();
 
     let mut grid: [[Cell; grid_config::COLUMN_COUNT as usize]; grid_config::ROW_COUNT as usize] =
@@ -154,6 +149,7 @@ fn main() {
         }
     }
 
+    /*
     grid[0][0].current_state = 1;
     grid[1][1].current_state = 1;
     grid[2][2].current_state = 1;
@@ -166,6 +162,7 @@ fn main() {
     );
     grid[0][0] = cell_tick(grid[0][0], grid);
     println!("current_state: {}", grid[0][0].current_state);
+    */
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -185,7 +182,6 @@ fn main() {
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -196,15 +192,11 @@ fn main() {
         }
 
         // Render here
-        //i = (i + 1) % 255;
-        //canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        //canvas.clear();
         canvas.set_draw_color(Color::RGB(255, 255, 255));
         canvas.clear();
 
         for row in 0..grid_config::ROW_COUNT {
             for column in 0..grid_config::COLUMN_COUNT {
-                //if rng.gen_range(0, 2) == 0
                 if grid[row as usize][column as usize].current_state == 1 {
                     canvas.set_draw_color(Color::RGB(0, 0, 0));
                     let cell_size: i32 = grid_config::CELL_SIZE;
@@ -222,17 +214,13 @@ fn main() {
 
         for row in 0..grid_config::ROW_COUNT {
             for column in 0..grid_config::COLUMN_COUNT {
-                //println!("{:#?}", grid[row as usize][column as usize]);
                 grid[row as usize][column as usize] =
                     cell_swap(grid[row as usize][column as usize]);
-                //println!("{:#?}", grid[row as usize][column as usize]);
             }
         }
 
         canvas.present();
 
         ::std::thread::sleep(Duration::from_millis(20));
-        //println!("next step: {}", step_count);
-        step_count += 1;
     }
 }
