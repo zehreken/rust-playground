8c41843ea65ad5bbd7556c131349c7efdb03df16
zehreken <zehreken@gmail.com>
2019-11-17 23:36:03 +0100

    Add String slice tests

--- src/ownership/mod.rs
+++ src/ownership/mod.rs
@@ -1,5 +1,16 @@
 pub fn start_ownership() {
-    let s = String::from("hello");
+    let l = "literal"; // A string literal is a slice, hence a &str, immutable
+    let s = String::from("hello world");
+
+    let hello = &s[0..5]; // String slice
+    let world = &s[6..11]; // It is reference to a part of a String
+                           // Also &[0..n] is equal to &[..n], the zero can be dropped
+                           // and &[n..len] is equal to &[n..], the last index can be dropped
+                           // and &[0..len] is equal to &[..], if you get the entire slice
+    println!("{}, {}", hello, world);
+
+    let word = first_word(&s); // Here s is borrowed
+    println!("{}", word);
 
     takes_ownership(s);
 
@@ -19,6 +30,18 @@ pub fn start_ownership() {
     makes_copy(x);
 }
 
+// fn first_word(s: &String) -> &str { A better way of doing this is
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
 fn takes_ownership(some_string: String) {
     println!("{}", some_string);
 }
@@ -27,14 +50,13 @@ fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
 }
 
-fn gives_ownership() -> String
-{
+fn gives_ownership() -> String {
     let s = String::from("hello");
 
     return s;
 }
 
-fn takes_and_gives_ownership(some_string:  String) -> String {
+fn takes_and_gives_ownership(some_string: String) -> String {
     println!("{}", some_string);
 
     return some_string;
