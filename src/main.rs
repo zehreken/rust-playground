9c2a7581c6a38648307ebe7649341ad546a84bf1
zehreken <zehreken@gmail.com>
2019-11-25 00:25:40 +0100

    Add options

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 extern crate sdl2;
+use std::env;
 
 mod automata;
 pub use crate::automata::*;
@@ -14,10 +15,30 @@ pub use crate::ownership::*;
 mod concurrency;
 pub use crate::concurrency::*;
 
+const AUTOMATA: &str = "--automata";
+const FAST_TYPE: &str = "--fast_type";
+const FRAMEBUFFER: &str = "--framebuffer";
+const OWNERSHIP: &str = "--ownership";
+const CONCURRENCY: &str = "--concurrency";
+
 fn main() {
-    // start_automata();
-    // start_fast_type();
-    // start_framebuffer();
-    // start_ownership();
-    start_concurrency();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!(
+            "OPTIONS:\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}",
+            AUTOMATA, FAST_TYPE, FRAMEBUFFER, OWNERSHIP, CONCURRENCY
+        );
+    } else if args.len() > 2 {
+        println!("Too many arguments!");
+    } else {
+        let arg: &str = &args[1][..];
+        match arg {
+            AUTOMATA => start_automata(),
+            FAST_TYPE => start_fast_type(),
+            FRAMEBUFFER => start_framebuffer(),
+            OWNERSHIP => start_ownership(),
+            CONCURRENCY => start_concurrency(),
+            _ => println!("Unknown arguement!"),
+        }
+    }
 }
