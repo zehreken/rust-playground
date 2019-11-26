0fbff82eb4faf8f2c72ece3c7eee17ac299c72b8
zehreken <zehreken@gmail.com>
2019-11-26 21:32:17 +0100

    Create window and render color using OpenGL

--- src/opengl_test/mod.rs
+++ src/opengl_test/mod.rs
@@ -1,1 +1,45 @@
-pub fn start_opengl_test() {}
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::surface::Surface;
+use sdl2::video::GLProfile;
+use std::time::{Duration, Instant};
+
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+
+pub fn start_opengl_test() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+
+    let window = video_subsystem
+        .window("opengl_test", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let context = window.gl_create_context().unwrap();
+    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        unsafe {
+            gl::ClearColor(1.0, 0.0, 0.21, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }
+
+        std::thread::sleep(Duration::from_millis(20));
+    }
+}
