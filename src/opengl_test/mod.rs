541e03d37354a2980d3518ae2dd6b5d2e82499ea
zehreken <zehreken@gmail.com>
2019-11-28 22:29:45 +0100

    Draw the holy triangle

--- src/opengl_test/mod.rs
+++ src/opengl_test/mod.rs
@@ -1,11 +1,23 @@
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::surface::Surface;
 use sdl2::video::GLProfile;
+use std::ffi::{CStr, CString};
 use std::time::{Duration, Instant};
 
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
+
+// const vertices: [f32; 32] = [
+//     0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.6, 0.4, // top right
+//     0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.6, 0.6, // bottom right
+//     -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.4, 0.6, // bottom left
+//     -0.5, 0.5, 0.0, 1.0, 1.0, 1.0, 0.4, 0.4, // top left
+// ];
+
+// Triangle
+const VERTICES: [f32; 9] = [
+    -0.5, -0.5, 0.0, // left
+    0.5, -0.5, 0.0, // right
+    0.0, 0.5, 0.0, // top
+];
 
 pub fn start_opengl_test() {
     let sdl_context = sdl2::init().unwrap();
@@ -24,22 +36,111 @@ pub fn start_opengl_test() {
     let context = window.gl_create_context().unwrap();
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
 
+    // Shader creation
+    // vertex shader
+    let vertex_shader_source: CString = CString::new("#version 330 core\nlayout (location = 0) in vec3 aPos;\nvoid main()\n{\ngl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}\n")
+    .expect("CString::new failed");
+
+    let fragment_shader_source: CString = CString::new("#version 330 core\nout vec4 FragColor;\nvoid main()\n{\nFragColor = vec4(0.0f, 0.0f, 0.2f, 1.0f);\n}\n")
+    .expect("CString::new failed");
+
+    let mut vertex_shader: u32 = 0;
+    let mut fragment_shader: u32 = 0;
+    let mut shader_program: u32 = 0;
+    unsafe {
+        vertex_shader = shader_from_source(&vertex_shader_source, gl::VERTEX_SHADER).unwrap();
+        fragment_shader = shader_from_source(&fragment_shader_source, gl::FRAGMENT_SHADER).unwrap();
+        shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+        // check for linking errors LATER
+        // gl::GetProgramiv(shader_program, gl::LINK_STATUS, &success);
+        // if (!success) {
+        //     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        //     std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        // }
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+    }
+    // ===============
+
+    let mut vao: u32 = 0;
+    let mut vbo: u32 = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (VERTICES.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+            VERTICES.as_ptr() as *const gl::types::GLvoid,
+            gl::STATIC_DRAW,
+        );
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (3 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            std::ptr::null(),
+        );
+        gl::EnableVertexAttribArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    'running: loop {
+    'game: loop {
         unsafe {
             gl::ClearColor(1.0, 0.0, 0.21, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         window.gl_swap_window();
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit { .. } => break 'running,
+                sdl2::event::Event::Quit { .. } => break 'game,
                 _ => {}
             }
         }
 
         std::thread::sleep(Duration::from_millis(20));
     }
+}
+
+fn shader_from_source(source: &CStr, kind: gl::types::GLuint) -> Result<gl::types::GLuint, String> {
+    let id = unsafe { gl::CreateShader(kind) };
+
+    unsafe {
+        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+        gl::CompileShader(id);
+    }
+
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut len: gl::types::GLint = 0;
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        }
+
+        let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
+        buffer.extend([b' '].iter().cycle().take(len as usize));
+        let error: CString = unsafe { CString::from_vec_unchecked(buffer) };
+        [b' '].iter().cycle().take(len as usize);
+    }
+
+    Ok(id)
 }
