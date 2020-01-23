6a8c51b1bac5368c5a9920226444b97821e76c69
zehreken <zehreken@gmail.com>
2020-01-23 23:09:02 +0100

    Add examples about ownership

--- src/rust_book/mod.rs
+++ src/rust_book/mod.rs
@@ -3,9 +3,77 @@ use std::cmp::Ordering;
 use std::io;
 
 pub fn start_rust_book() {
-    chapter_3();
+    chapter_4();
+    // chapter_3();
     // chapter_2();
 }
+
+fn chapter_4() {
+    // Very basic example for scope
+    {
+        let s = 3;
+    }
+    // println!("{}", s); // Throws error
+    let mut s = String::from("hello");
+    s.push_str(", world");
+    println!("{}", s);
+
+    let i1 = 5;
+    let i2 = i1; // No move happens, just copy because i1 and i2 are fixed size variables
+    println!("{}", i1);
+
+    let s1 = String::from("move me");
+    let s2 = s1; // Move happens
+                 // println!("{}", s1); // Gives error 'value used here after move'
+
+    let s3 = String::from("copy me");
+    let s4 = s3.clone();
+    println!("{}, {}", s3, s4);
+
+    let s = String::from("hello"); // s comes into scope
+
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+
+    let x = 5; // x comes into scope
+
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it’s okay to still
+                   // use x afterward
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope
+
+    a_string // a_string is returned and moves out to the calling function
+}
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+}
+// CHAPTER 4
 
 fn chapter_3() {
     println!("CHAPTER 3");
