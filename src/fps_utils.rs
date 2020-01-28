c0fc653cd4f34175e71be8fd96b87c7ca362b629
zehreken <zehreken@gmail.com>
2020-01-28 23:52:46 +0100

    Rename field

--- src/fps_utils.rs
+++ src/fps_utils.rs
@@ -1,21 +1,21 @@
 pub struct FpsCounter {
-    time_as_second: u128,
+    time_as_ms: u128,
     frames: i32,
 }
 
 impl FpsCounter {
     pub fn new() -> FpsCounter {
         FpsCounter {
-            time_as_second: 0,
+            time_as_ms: 0,
             frames: 0,
         }
     }
 
     pub fn tick(&mut self, delta_ms: u128) -> i32 {
-        self.time_as_second += delta_ms;
+        self.time_as_ms += delta_ms;
         self.frames += 1;
-        if self.time_as_second >= 1000 {
-            self.time_as_second -= 1000;
+        if self.time_as_ms >= 1000 {
+            self.time_as_ms -= 1000;
             let temp = self.frames;
             self.frames = 0;
             return temp;
