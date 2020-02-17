d00a2186ca3fffffffae1a8deedca24320656930
zehreken <zehreken@gmail.com>
2020-02-17 22:52:39 +0100

    Add chapter 12 examples

--- src/rust_book/chapter_12.rs
+++ src/rust_book/chapter_12.rs
@@ -2,5 +2,10 @@ use std::env;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
-    println!("arguments: {:?}", args);
+
+    let query = &args[2];
+    let filename = &args[3];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
