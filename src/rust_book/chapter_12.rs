ed6d0b91a5acdc11e39e387a97764328e63edc65
zehreken <zehreken@gmail.com>
2020-02-18 23:33:40 +0100

    Add chapter 12 examples

--- src/rust_book/chapter_12.rs
+++ src/rust_book/chapter_12.rs
@@ -1,16 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[2];
-    let filename = &args[3];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 4 {
+            return Err("not enough arguments");
+        }
+        let query = args[2].clone();
+        let filename = args[3].clone();
+
+        Ok(Config { query, filename })
+    }
 }
