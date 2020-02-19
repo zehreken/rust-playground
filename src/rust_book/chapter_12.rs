806282e4981d7e0c14161c7674ed1715b30f1a08
zehreken <zehreken@gmail.com>
2020-02-19 21:12:46 +0100

    Add chapter 12 examples

--- src/rust_book/chapter_12.rs
+++ src/rust_book/chapter_12.rs
@@ -10,11 +10,25 @@ pub fn run() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = crate::rust_book::lib::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+save, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["save, fast, productive."],
+            crate::rust_book::lib::search(query, contents)
+        );
     }
 }
