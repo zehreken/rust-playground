66b33670b4fe8f36ea3a423d030da4daf8ba8576
zehreken <zehreken@gmail.com>
2020-02-22 14:13:25 +0100

    Add chapter 13 examples

--- src/rust_book/chapter_13.rs
+++ src/rust_book/chapter_13.rs
@@ -1,3 +1,33 @@
+use std::thread;
+use std::time::Duration;
+
 pub fn run() {
-    println!("Chapter 13 starts");
+    let simulated_use_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_use_specified_value, simulated_random_number);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_closure(intensity))
+        }
+    }
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
