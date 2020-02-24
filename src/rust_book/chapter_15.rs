085287658a1af74c8d35f6e2975915f950ae1a56
zehreken <zehreken@gmail.com>
2020-02-25 00:08:01 +0100

    Add chapter 15 examples

--- src/rust_book/chapter_15.rs
+++ src/rust_book/chapter_15.rs
@@ -5,9 +5,47 @@ pub fn run() {
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // *(y.deref()) is actually run here
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
 
 enum List {
     Cons(i32, Box<List>), // By using a box , we've broken the infinite, recursive chain
     Nil,
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
