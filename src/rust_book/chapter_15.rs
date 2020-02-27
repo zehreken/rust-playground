c7a18960eb6d0a191ae818bb0599605ba3e0255c
zehreken <zehreken@gmail.com>
2020-02-27 22:39:28 +0100

    Add chapter 15 examples

--- src/rust_book/chapter_15.rs
+++ src/rust_book/chapter_15.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 // use List::{Cons, Nil};
-use List2::{Cons, Nil};
+// use List2::{Cons, Nil};
 
 pub fn run() {
     let b = Box::new(5);
@@ -37,17 +37,39 @@ pub fn run() {
     println!("CustomSmartPointers created.");
 
     // Rc<T> trait
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
     // Interior mutability RefCell<T>
+    // Creating a reference cycle
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b  initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle
+    // it will overflow the stack
+    println!("a next item = {:?}", a.tail());
 }
 
 pub trait Messenger {
@@ -129,6 +151,23 @@ struct CustomSmartPointer {
 impl Drop for CustomSmartPointer {
     fn drop(&mut self) {
         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+use std::cell::RefCell;
+use List3::{Cons, Nil};
+#[derive(Debug)]
+enum List3 {
+    Cons(i32, RefCell<Rc<List3>>),
+    Nil,
+}
+
+impl List3 {
+    fn tail(&self) -> Option<&RefCell<Rc<List3>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
     }
 }
 
