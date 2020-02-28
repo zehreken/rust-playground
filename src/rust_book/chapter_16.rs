ed7b958b0d0242c32f326b0db51ea82bb0cd99b1
zehreken <zehreken@gmail.com>
2020-02-28 22:52:23 +0100

    Add chapter 16 examples

--- src/rust_book/chapter_16.rs
+++ src/rust_book/chapter_16.rs
@@ -1,1 +1,50 @@
-pub fn run() {}
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+pub fn run() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Using move
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+
+    // Channels
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    // let received = rx.recv().unwrap();
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
