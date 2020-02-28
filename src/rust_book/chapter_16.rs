3ea5ea1e653f901dee38d41da9298cde4089efb6
zehreken <zehreken@gmail.com>
2020-02-28 23:16:31 +0100

    Add chapter 16 examples

--- src/rust_book/chapter_16.rs
+++ src/rust_book/chapter_16.rs
@@ -28,6 +28,7 @@ pub fn run() {
 
     // Channels
     let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
 
     thread::spawn(move || {
         let vals = vec![
@@ -39,6 +40,20 @@ pub fn run() {
 
         for val in vals {
             tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
