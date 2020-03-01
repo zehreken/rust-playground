9acfaa018710e89a50187a449285753cd4f9d749
zehreken <zehreken@gmail.com>
2020-03-01 23:39:11 +0100

    Add chapter 17 examples

--- src/rust_book/chapter_17.rs
+++ src/rust_book/chapter_17.rs
@@ -1,1 +1,34 @@
-pub fn run() {}
+use crate::rust_book::lib::{Button, Draw, Screen};
+
+pub fn run() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {}
+}
--- src/rust_book/lib.rs
+++ src/rust_book/lib.rs
@@ -66,3 +66,29 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
         .filter(|line| line.contains(&query))
         .collect()
 }
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {}
+}
