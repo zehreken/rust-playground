69983fbfdb0953b40a2a655b438e0eb4eb5e8394
zehreken <zehreken@gmail.com>
2020-03-07 23:25:08 +0100

    Add chapter 19 examples

--- src/rust_book/chapter_19.rs
+++ src/rust_book/chapter_19.rs
@@ -7,9 +7,34 @@ pub fn run() {
     advanced_traits();
 
     advanced_type();
+
+    advanced_functions();
 }
 
-// Advanced types
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn advanced_functions() {
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+}
+
 fn advanced_type() {
     type Kilometers = i32;
 
