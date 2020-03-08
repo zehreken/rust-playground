87e11ae0d80c8a7f059f392c5743bfc2760816d5
zehreken <zehreken@gmail.com>
2020-03-08 22:38:35 +0100

    Add chapter 20 examples

--- src/rust_book/chapter_20.rs
+++ src/rust_book/chapter_20.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -14,8 +15,21 @@ pub fn run() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUNT\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
