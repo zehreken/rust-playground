f02ae2fcd3b8349dd5e90ad89e8517ccdc8443e3
zehreken <zehreken@gmail.com>
2020-05-28 23:30:29 +0200

    Add more concurrency example

--- src/concurrency/mod.rs
+++ src/concurrency/mod.rs
@@ -1,20 +1,48 @@
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 pub fn start_concurrency() {
+    let now = Instant::now();
+    single_thread();
+    let diff = Instant::now() - now;
+    println!("{:?}", diff);
+
+    let now = Instant::now();
+    simple_threads();
+    let diff = Instant::now() - now;
+    println!("{:?}", diff);
+}
+
+fn single_thread() {
+    for j in 0..8 {
+        for i in 0..8 {
+            expensive_procedure();
+        }
+    }
+}
+
+fn simple_threads() {
     let mut handles = vec![];
     for j in 0..8 {
-        let mut v = vec![];
+        // let mut v = vec![];
         handles.push(thread::spawn(move || {
             for i in 0..8 {
-                v.push(j * 8 + i);
+                expensive_procedure();
             }
-            v
         }));
     }
 
     for h in handles {
         let v = h.join().unwrap(); // Calling join blocks the current thread from exiting
-        println!("{:?}", v);
     }
+}
+
+fn expensive_procedure() -> f32 {
+    const TRY_COUNT: i32 = 10_000_000;
+    let mut sum = 0.0;
+    for i in 0..TRY_COUNT {
+        sum += (i as f32).sin();
+    }
+
+    sum
 }
