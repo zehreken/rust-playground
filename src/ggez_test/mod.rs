4322211e022fa63a692181520cbe7ebf8f462f9e
zehreken <zehreken@gmail.com>
2020-07-06 18:43:25 +0200

    Add 'hello, world' example for ggez

--- src/ggez_test/mod.rs
+++ src/ggez_test/mod.rs
@@ -1,3 +1,5 @@
+use cgmath;
+use ggez;
 use ggez::event::{self, EventHandler};
 use ggez::{graphics, Context, ContextBuilder, GameResult};
 
@@ -10,37 +12,48 @@ pub fn run() {
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object to
     // use when setting your game up.
-    let mut my_game = MyGame::new(&mut ctx);
+    let mut game = MainState::new(&mut ctx);
 
     // Run!
-    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+    match event::run(&mut ctx, &mut event_loop, &mut game) {
         Ok(_) => println!("Exited cleanly."),
         Err(e) => println!("Error occured: {}", e),
     }
 }
 
-struct MyGame {
-    // Your state here...
+struct MainState {
+    frames: usize,
+    text: graphics::Text,
 }
 
-impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
-        // Load/create resources such as images here.
-        MyGame {
-		    // ...
-		}
+impl MainState {
+    pub fn new(ctx: &mut Context) -> MainState {
+        let font = graphics::Font::new(ctx, "/emulogic.ttf").unwrap();
+        let text = graphics::Text::new(("Hello World!", font, 18.0));
+
+        MainState { frames: 0, text }
     }
 }
 
-impl EventHandler for MyGame {
+impl EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         // Update code here...
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, graphics::WHITE);
-        // Draw code here...
+        graphics::clear(ctx, graphics::BLACK);
+
+        let offset = self.frames as f32 / 10.0;
+        let position = ggez::mint::Point2 {
+            x: offset,
+            y: offset,
+        };
+        graphics::draw(ctx, &self.text, (position,)).unwrap();
+
+        self.frames += 1;
+        if self.frames % 100 == 0 {}
+
         graphics::present(ctx)
     }
 }
